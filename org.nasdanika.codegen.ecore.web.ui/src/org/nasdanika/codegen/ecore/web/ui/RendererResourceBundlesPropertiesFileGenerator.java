b1b47803b21aff8027e10ab57603e21a5927df92
Pavel <Pavel.Vlasov@nasdanika.org>
2017-02-02 16:16:50 -0500

    Renderer properties generation.

--- org.nasdanika.codegen.ecore.web.ui/src/org/nasdanika/codegen/ecore/web/ui/RendererResourceBundlesPropertiesFileGenerator.java
+++ org.nasdanika.codegen.ecore.web.ui/src/org/nasdanika/codegen/ecore/web/ui/RendererResourceBundlesPropertiesFileGenerator.java
@@ -3,12 +3,20 @@ package org.nasdanika.codegen.ecore.web.ui;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.nasdanika.codegen.Work;
+import org.nasdanika.codegen.ecore.EcoreCodeGenerator;
 import org.nasdanika.config.Context;
 
 public class RendererResourceBundlesPropertiesFileGenerator implements Work<InputStream> {
@@ -20,17 +28,65 @@ public class RendererResourceBundlesPropertiesFileGenerator implements Work<Inpu
 
 	@Override
 	public InputStream execute(Context context, SubMonitor monitor) throws Exception {
+		EcoreCodeGenerator ecoreCodeGenerator = context.get(EcoreCodeGenerator.class);
 		EClass eClass = context.get(EClass.class);
 		Properties properties = new Properties();
-		String[] cca = StringUtils.splitByCharacterTypeCamelCase(eClass.getName());
-		for (int i=1; i<cca.length; ++i) {
-			cca[i] = cca[i].toLowerCase();
+		String className = eClass.getName();
+		properties.put("class.label", nameToLabel(className));
+		
+		List<EStructuralFeature> selectedFeatures = new ArrayList<>();
+		List<EOperation> selectedOperations= new ArrayList<>();
+		
+		// Own features and operations
+		for (EStructuralFeature esf: eClass.getEStructuralFeatures()) {
+			if (ecoreCodeGenerator.isSelected(esf)) {
+				selectedFeatures.add(esf);
+			}
 		}
-		properties.put("class.label", StringUtils.join(cca, " "));
+		for (EOperation op: eClass.getEOperations()) {
+			if (ecoreCodeGenerator.isSelected(op)) {
+				selectedOperations.add(op);
+			}
+		}
+		// Features and operations from not selected supertypes
+		collectFeaturesAndOperations(ecoreCodeGenerator, eClass, new HashSet<>(), selectedFeatures, selectedOperations);
+		
+		for (EStructuralFeature sf: selectedFeatures) {
+			properties.put("feature."+sf.getName()+".label", nameToLabel(sf.getName()));			
+		}
+		
+		for (EOperation op: selectedOperations) {
+			properties.put("operation."+op.getName()+".label", nameToLabel(op.getName()));			
+		}
+		
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		properties.store(baos, null);
 		baos.close();
 		return new ByteArrayInputStream(baos.toByteArray());
+	}
+	
+	private void collectFeaturesAndOperations(EcoreCodeGenerator ecoreCodeGenerator, EClass eClass, Set<EClass> traversed, Collection<EStructuralFeature> featureCollector, Collection<EOperation> operationCollector) {
+		if (traversed.add(eClass) && !ecoreCodeGenerator.isSelected(eClass)) {
+			for (EStructuralFeature esf: eClass.getEStructuralFeatures()) {
+				featureCollector.add(esf);
+			}
+			for (EOperation op: eClass.getEOperations()) {
+				operationCollector.add(op);
+			}
+			for (EClass st: eClass.getESuperTypes()) {
+				collectFeaturesAndOperations(ecoreCodeGenerator, st, traversed, featureCollector, operationCollector);
+			}
+		}
+	}
+
+	private String nameToLabel(String name) {
+		String[] cca = StringUtils.splitByCharacterTypeCamelCase(name);
+		cca[0] = StringUtils.capitalize(cca[0]);
+		for (int i=1; i<cca.length; ++i) {
+			cca[i] = cca[i].toLowerCase();
+		}
+		String classLabel = StringUtils.join(cca, " ");
+		return classLabel;
 	}
 
 }
